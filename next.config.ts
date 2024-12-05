5f6e8a340ff7c3159d2467820de387234b1fccdf
Parth-12pm <parth_sanjay.mahadik@vpt.edu.in>
2024-12-05 23:55:14 +0530

    Enhance playground functionality by adding a dynamic canvas component and updating Next.js configuration. Updated 'lucide-react' dependency to version 0.468.0 for improved icon support.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { canvas: 'canvas' }]; // required to make Konva & react-konva work
+    return config;
+  },
 }
 
 export default nextConfig;
