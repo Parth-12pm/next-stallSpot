73ad1b22914fd9478a24f4d9d626aba106ab9794
Parth-12pm <parth_sanjay.mahadik@vpt.edu.in>
2024-12-28 09:29:41 +0530

    deleted one file

--- pages/api/auth/[...nextauth].ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from 'next-auth'
-import { NextAuthOptions } from 'next-auth'
-
-export const authOptions: NextAuthOptions = {
-  secret: process.env.JWT_SECRET,
-  // Add your providers and callbacks here
-}
-
-export default NextAuth(authOptions)
