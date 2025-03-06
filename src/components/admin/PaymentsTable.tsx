0b847400c7007c359980c517486b0d4210dc9cf1
Parth-12pm <parth_sanjay.mahadik@vpt.edu.in>
2025-03-06 21:46:04 +0530

    chore: disable unused variable linting in admin table components and useEvents hook

--- src/components/admin/PaymentsTable.tsx
+++ src/components/admin/PaymentsTable.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+
 import { useState, useEffect } from "react"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
